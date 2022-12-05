7247c6f1f28871af36650eac152a3ba8e8488ed8
jeein2222 <jiinbag057@gmail.com>
2022-12-05 14:30:59 +0900

    문자열 뒤집어 문자열로 리턴하기

--- ReverseString.java
+++ ReverseString.java
@@ -1,2 +1,29 @@
-package codingTest;public class ReverseString {
+package codingTest;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//문자열 뒤집어 문자열로 리턴
+public class ReverseString {
+    public static String solution1(String my_string) {
+        String answer = "";
+        List<String> stringList= Arrays.asList(my_string.split(""));
+        Collections.reverse(stringList);
+        //String.join은 List에서도 가능하다.
+        answer=String.join("",stringList);
+        return answer;
+    }
+
+    public static String solution2(String my_string){
+        StringBuilder sb=new StringBuilder();
+        sb.append(my_string);
+        sb.reverse();
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution2("hello"));
+    }
 }
