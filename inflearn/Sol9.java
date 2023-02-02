e0e9a5ff6b6d1d855e47598165f8419c98c43c47
jeein2222 <jiinbag057@gmail.com>
2023-02-02 13:57:23 +0900

    숫자만 추출

--- inflearn/Sol9.java
+++ inflearn/Sol9.java
@@ -3,8 +3,9 @@ package codingTest.inflearn;
 import java.util.Scanner;
 
 //유효한
-public class Sol8 {
-    public int solution(String s){
+public class Sol9 {
+    //mine
+    public int solution1(String s){
         char[] arr=s.toCharArray();
         String answer="";
         for(int i=0;i<arr.length;i++){
@@ -12,12 +13,24 @@ public class Sol8 {
                 answer+=arr[i];
         }
         return Integer.valueOf(answer);
+        //return Integer.parseInt(answer);
+    }
+    //answer
+    public int solution2(String s){
+        char[] arr=s.toCharArray();
+        int answer=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>=48 && arr[i]<=57)//0~9숫자인 경우
+                answer=answer*10+(arr[i]-48);
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
-        Sol8 s=new Sol8();
+        Sol9 s=new Sol9();
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        System.out.println(s.solution(str));
+        System.out.println(s.solution1(str));
+        System.out.println(s.solution2(str));
     }
 }
