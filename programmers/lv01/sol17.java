1df941467e086fd5e602b864d60e4c13dae41d78
jeein2222 <jiinbag057@gmail.com>
2023-02-10 18:59:20 +0900

    숫자 문자열과 영단어

--- programmers/lv01/sol17.java
+++ programmers/lv01/sol17.java
@@ -1,2 +1,18 @@
-package codingTest.programmers.lv01;public class sol17 {
+package codingTest.programmers.lv01;
+
+/*
+    숫자 문자열과 영단어
+ */
+public class sol17 {
+    public int solution(String s) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        sol17 s=new sol17();
+        System.out.println(s.solution("one4seveneight"));
+        System.out.println(s.solution("23four5six7"));
+        System.out.println(s.solution("2three45sixseven"));
+        System.out.println(s.solution("123"));
+    }
 }
