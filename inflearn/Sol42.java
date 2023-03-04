fc7a3902126950a765b8bae316dbde4c6464782b
jeein2222 <jiinbag057@gmail.com>
2023-03-04 17:32:07 +0900

    재귀함수 10진수를 2진수로

--- inflearn/Sol42.java
+++ inflearn/Sol42.java
@@ -1,2 +1,23 @@
-package codingTest.inflearn;public class Sol42 {
+package codingTest.inflearn;
+
+//재귀함수를 이용해 10진수를 2진수로 바꾸어보세요.
+public class Sol42 {
+    public void DFS(int n){
+        if(n==0) return;
+        else{
+            DFS(n/2);
+            System.out.print(n%2);
+        }
+    }
+    public static void main(String[] args) {
+        Sol42 s = new Sol42();
+        s.DFS(6);
+    }
+    /*
+        6/2=3 ...0
+        3/2=1 ...1
+        1/2=0 ...1
+
+        6 -> 110
+     */
 }
